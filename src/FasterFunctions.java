8ecea819652af566c220f896bcf83de7a834956a
root <root@031e3c7a3ba4>
2020-11-06 02:30:39 +0000

    bdt

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -1,9 +1,27 @@
 
-public class FasterFunctions {
+public class FasterFunctions implements Runnable {
 	public static void main(String[] args) {
 		float totalTime = 0;
 		long startTime = System.currentTimeMillis();
-		//Your Code Here
+		Thread threadOne=new Thread(()-> {
+			SlowFunctions.slowSortLargeArray();
+		});
+		Thread threadTwo=new Thread(()-> {
+			SlowFunctions.ackermann(3, 14);
+		});
+		Thread threadThree=new Thread(()-> {
+			SlowFunctions.millionsOfSqrts();
+		});
+		threadOne.start();
+		threadTwo.start();
+		threadThree.start();
+		try {
+			threadOne.join();
+			threadTwo.join();
+			threadThree.join();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 		
@@ -11,5 +29,11 @@ public class FasterFunctions {
 		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
 		totalTime += endTime;
 		System.out.println("Total Time: " + totalTime);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 }
